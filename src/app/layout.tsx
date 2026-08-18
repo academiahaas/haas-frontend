f1655d63f140ad0228bc1e66fca1ffdd8f946063
Your Name <you@example.com>
2026-08-18 22:43:14 +0200

    atualização 2026-08-18_22:43

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import "./globals.css";
 
 export const metadata = {
+  icons: { icon: "https://jdppxfokfhqjudwfwckd.supabase.co/storage/v1/object/public/haas-academy/assignments/Melhorar%20Logo%20(3).png" },
   title: "Academia Haas",
   description: "Portal de Ensino Síncrono Avançado",
 };
